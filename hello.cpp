44f12ee49da1bbbf15c53d8abc3e0c024e7e6492
AymanALi <ayman.405060@gmail.com>
2023-11-25 21:22:40 +0300

    edit delay to 2

--- hello.cpp
+++ hello.cpp
@@ -12,7 +12,7 @@ void matchstring(string a,string b){
 	for(int i = 0;i<b.length();i++){
 		for(int j = 0;j<a.length();j++){
 			cout<<a[j];
-			delay(1);
+			delay(2);
 			cout<<"\b \b";
 			if(a[j]==b[i]){
 			cout<<a[j];
